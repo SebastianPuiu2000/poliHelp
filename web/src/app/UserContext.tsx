e93e5331bbb9a6ef34994dc26e44f088d310cf63
mihneabuz <mihneabuzatu88@gmail.com>
2023-03-04 12:45:04 +0200

    added user provider context

---
+++ web/src/app/UserContext.tsx
@@ -0,0 +1,54 @@
+import { createContext, Dispatch, useContext, useReducer } from 'react';
+
+interface User {
+  name: string,
+  type: string
+}
+
+interface UserAction {
+  type: UserActionType,
+  payload: any
+}
+
+enum UserActionType {
+  login,
+  logout
+}
+
+const UserContext = createContext<User | null>(null);
+const UserDispatchContext = createContext<Dispatch<UserAction> | null>(null);
+
+export function UserProvider({ children }) {
+  const [user, dispatch] = useReducer(
+    userReducer,
+    null
+  );
+
+  return (
+    <UserContext.Provider value={user}>
+      <UserDispatchContext.Provider value={dispatch}>
+        {children}
+      </UserDispatchContext.Provider>
+    </UserContext.Provider>
+  );
+}
+
+export function useTasks() {
+  return useContext(UserContext);
+}
+
+export function useTasksDispatch() {
+  return useContext(UserDispatchContext);
+}
+
+function userReducer(user: User | null, action: UserAction): User | null {
+  switch (action.type) {
+    case UserActionType.login:
+      console.log('login!');
+      return action.payload.user;
+
+    case UserActionType.logout:
+      console.log('logout');
+      return null;
+  }
+}
--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,4 +1,6 @@
 import './globals.css'
+import { UserProvider } from './UserContext'
+
 
 export const metadata = {
   title: 'App',
@@ -8,7 +10,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <UserProvider>
+          {children}
+        </UserProvider>
+      </body>
     </html>
   )
 }
