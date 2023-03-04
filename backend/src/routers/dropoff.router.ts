d41a8c55a3bf625e97c911fe4f3f80c89e7ce0e1
Niko18500 <andreinicolescu3@gmail.com>
2023-03-04 19:03:30 +0200

    Fix dropoff route

--- backend/src/routers/dropoff.router.ts
+++ backend/src/routers/dropoff.router.ts
@@ -1,7 +1,7 @@
 import * as express from 'express';
 import { getToken, verify } from '../jwt';
 import { JwtPayload } from 'jsonwebtoken';
-import { UserModel } from '../models/user.model';
+import { userRole } from '../models/user.model';
 import { Dropoff, DropoffModel } from '../models/dropoff.model';
 
 export const dropoffRouter = express.Router();
@@ -9,10 +9,17 @@ export const dropoffRouter = express.Router();
 // Get dropoff points around a given location
 dropoffRouter.get('/', async (req, res) => {
     let token: string = getToken(req.url);
+    let payload: JwtPayload = verify(token);
     
-    if (token === '') {
+    if (token === '' || !payload) {
         return res
             .status(400)
+            .json({success: false, dropoffs: []});
+    }
+
+    if (payload.role !== userRole.Delivery) {
+        return res
+            .status(401)
             .json({success: false, dropoffs: []});
     }
     
@@ -24,16 +31,6 @@ dropoffRouter.get('/', async (req, res) => {
     let requestLat = Number(req.query.lat);
     let requestLgn = Number(req.query.lgn);
 
-    let payload: JwtPayload = verify(token);
-
-    if (!payload) {
-        return res
-            .status(400)
-            .json({success: false, dropoffs: []});
-    }
-
-    let mongoUser = await UserModel.findById(payload.id);
-
     let nearbyDropoffs = await DropoffModel.find({
         $and: [
             {lat: {$gte: requestLat - 1, $lte: requestLat + 1}},
@@ -44,25 +41,25 @@ dropoffRouter.get('/', async (req, res) => {
     return res.json({success: true, dropoffs: nearbyDropoffs});
 });
 
+// Add dropoff
 dropoffRouter.post('/', async (req, res) => {
     let token: string = getToken(req.url);
+    let payload: JwtPayload = verify(token);
     
-    if (token === '') {
+    if (token === '' || !payload) {
         return res
             .status(400)
+            .json({success: false});
+    }
+
+    if (payload.role !== userRole.Delivery) {
+        return res
+            .status(401)
             .json({success: false});
     }
 
     let dropoff: Dropoff = req.body;
     if (!dropoff || !dropoff.lat || !dropoff.lgn) {
-        return res
-            .status(400)
-            .json({success: false});
-    }
-
-    let payload: JwtPayload = verify(token);
-
-    if (!payload) {
         return res
             .status(400)
             .json({success: false});
