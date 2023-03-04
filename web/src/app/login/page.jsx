ac8cbdaf39a37f85ddec3958e09fa99b88bf47cc
SEBI <sebastian.puiu2000@gmail.com>
2023-03-04 18:49:42 +0200

    Inca inca ceva

--- web/src/app/login/page.jsx
+++ web/src/app/login/page.jsx
@@ -8,6 +8,30 @@ import help from "../../../public/help.png"
 export default function Login() {
 
   const user = useUser();
+
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const form = {
+      name,
+      password
+    };
+
+    console.log(form);
+
+    const response = await fetch('/api/login', {
+      method: 'POST',
+      body: JSON.stringify(form)
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+  }
+
   return (
 
     <div className="bg-violet-900">
@@ -19,12 +43,23 @@ export default function Login() {
             <div className="flex items-center justify-center mb-20">
               <Image src={help} width={500} height={500} alt=""></Image>
             </div>
-            <input className="w-full text-center rounded-2xl py-2 bg-gray-50 text-black px-1 outline-none mb-6" type="name" placeholder="Name"></input>
-            <input className="w-full text-center rounded-2xl py-2 bg-gray-50 text-black px-1 outline-none mb-4" type="password" placeholder="Password"></input>
+            <input
+              className="w-full text-center rounded-2xl py-2 bg-gray-50 text-black px-1 outline-none mb-6" type="name" placeholder="Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <input
+              className="w-full text-center rounded-2xl py-2 bg-gray-50 text-black px-1 outline-none mb-4" type="password" placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </div>
         </form>
 
-        <button className="bg-black text-white w-28 text-center rounded-full py-2 px-1 outline-none mb-8">
+        <button className="bg-black text-white w-28 text-center rounded-full py-2 px-1 mb-8 hover:outline"
+          disabled={secondOption === null}
+          onClick={handleSubmit}
+        >
           Submit
         </button>
       </div>
--- web/src/app/register/page.jsx
+++ web/src/app/register/page.jsx
@@ -7,134 +7,136 @@ import Image from "next/image";
 
 
 export default function Register() {
-  const [firstOption, setFirstOption] = useState(null);
-  const [secondOption, setSecondOption] = useState(null);
+    const [firstOption, setFirstOption] = useState(null);
+    const [secondOption, setSecondOption] = useState(null);
 
-  const [name, setName] = useState('');
-  const [password, setPassword] = useState('');
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
 
-  const handleProvideHelpClick = () => {
-    setFirstOption('provide');
-  }
+    const handleProvideHelpClick = () => {
+        setFirstOption('provide');
+    }
 
-  const handleNeedHelpClick = () => {
-    setFirstOption('require');
-  }
+    const handleNeedHelpClick = () => {
+        setFirstOption('require');
+    }
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+    const handleSubmit = async (e) => {
+        e.preventDefault();
 
-    const form = {
-      name,
-      password,
-      role: secondOption
-    };
+        const form = {
+            name,
+            password,
+            role: secondOption
+        };
 
-    console.log(form);
+        console.log(form);
 
-    const response = await fetch('/api/register', {
-      method: 'POST',
-      body: JSON.stringify(form)
-    });
+        const response = await fetch('/api/register', {
+            method: 'POST',
+            body: JSON.stringify(form)
+        });
 
-    const data = await response.json();
+        const data = await response.json();
 
-    console.log(data);
-  }
+        console.log(data);
+    }
 
-  return (
+    return (
 
-    <div className="bg-violet-900">
-      <Navbar></Navbar>
+        <div className="bg-violet-900">
+            <Navbar></Navbar>
 
-      <div className="flex flex-col items-center justify-evenly bg-violet-900 m-10">
-        <div className="w-60 my-5">
+            <div className="flex flex-col items-center justify-evenly bg-violet-900 m-10">
+                
+                {/* Group 1 */}
+                <div className="w-60 my-5">
 
-          <button className="text-center text-white bg-black my-2 p-5 w-full rounded-full hover:outline"
-            onClick={handleProvideHelpClick}
-          >
-            I want to help!
-          </button>
-        </div>
+                    <button className="text-center text-white bg-black my-2 p-5 w-full rounded-full hover:outline"
+                        onClick={handleProvideHelpClick}
+                    >
+                        I want to help!
+                    </button>
 
-        <div className="w-60 my-5">
-          <button
-            className="text-center text-white bg-black my-2 p-5 w-full rounded-full hover:outline"
-            onClick={handleNeedHelpClick}
-          >
-            I need help!
-          </button>
-        </div>
-
-        <div className="w-60 my-10" hidden={firstOption !== 'provide'}>
-          <button
-            className="text-center text-white bg-black my-5 p-5 w-full rounded-full hover:outline"
-            onClick={() => setSecondOption('donate')}
-          >
-            I want to make donations
-          </button>
-
-          <button
-            className="text-center text-white bg-black my-5 p-5 w-full rounded-full hover:outline"
-            onClick={() => setSecondOption('delivery')}
-          >
-            I want to deliver donations
-          </button>
-
-          <button
-            className="text-center text-white bg-black my-5 p-5 w-full rounded-full hover:outline"
-            onClick={() => setSecondOption('provideShelter')}
-          >
-            I want to provide shelter
-          </button>
-        </div>
-
-        <div className="w-60 my-10" hidden={firstOption !== 'require'}>
-          <button
-            className="text-center text-white bg-black my-5 p-5 w-full rounded-full hover:outline"
-            onClick={() => setSecondOption('needSupplies')}
-          >
-            I need supplies
-          </button>
-
-          <button
-            className="text-center text-white bg-black my-5 p-5 w-full rounded-full hover:outline"
-            onClick={() => setSecondOption('needShelter')}
-          >
-            I need shelter
-          </button>
-        </div>
-
-        <div className="w-60 my-10" hidden={firstOption === null || secondOption === null}>
-          <div className="flex items-center justify-center bg-violet-900">
-            <form>
-              <div className="bg-violet-900 w-96 p-6 py-20 rounded shadow-sm">
-                <div className="flex items-center justify-center mb-20">
-                  <Image src={help} width={500} height={500} alt=""></Image>
+                    <button
+                        className="text-center text-white bg-black my-2 p-5 w-full rounded-full hover:outline"
+                        onClick={handleNeedHelpClick}
+                    >
+                        I need help!
+                    </button>
                 </div>
-                <label className="text-white"> Name </label>
-                <input
-                  className="w-full text-center rounded-2xl py-2 bg-gray-50 text-black px-1 outline-none mb-10" type="email"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                />
-                <label className="text-white"> Password </label>
-                <input
-                  className="w-full text-center rounded-2xl py-2 bg-gray-50 text-black px-1 outline-none mb-4" type="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-                <button
-                  disabled={secondOption === null}
-                  onClick={handleSubmit}
-                >
-                  Submit
-                </button>
-              </div>
-            </form>
-          </div>
+
+                {/* Group 2.1 */}
+                <div className="w-60 my-10" hidden={firstOption !== 'provide'}>
+                    <button
+                        className="text-center text-white bg-black my-2 p-5 w-full rounded-full hover:outline"
+                        onClick={() => setSecondOption('donate')}
+                    >
+                        I want to make donations
+                    </button>
+
+                    <button
+                        className="text-center text-white bg-black my-2 p-5 w-full rounded-full hover:outline"
+                        onClick={() => setSecondOption('delivery')}
+                    >
+                        I want to deliver donations
+                    </button>
+
+                    <button
+                        className="text-center text-white bg-black my-2 p-5 w-full rounded-full hover:outline"
+                        onClick={() => setSecondOption('provideShelter')}
+                    >
+                        I want to provide shelter
+                    </button>
+                </div>
+
+                {/* Group 2.2 */}
+                <div className="w-60 my-10" hidden={firstOption !== 'require'}>
+                    <button
+                        className="text-center text-white bg-black my-2 p-5 w-full rounded-full hover:outline"
+                        onClick={() => setSecondOption('needSupplies')}
+                    >
+                        I need supplies
+                    </button>
+
+                    <button
+                        className="text-center text-white bg-black my-2 p-5 w-full rounded-full hover:outline"
+                        onClick={() => setSecondOption('needShelter')}
+                    >
+                        I need shelter
+                    </button>
+                </div>
+
+                {/* Group 3 */}
+                <div className="w-60 my-10" hidden={firstOption === null || secondOption === null}>
+                    <div className="flex flex-col items-center justify-center bg-violet-900">
+                        <form>
+                            <div className="bg-violet-900 w-96 p-6 py-10 rounded shadow-sm">
+                                <div className="flex items-center justify-center mb-20">
+                                    <Image src={help} width={500} height={500} alt=""></Image>
+                                </div>
+                                <input
+                                    className="w-full text-center rounded-2xl py-2 bg-gray-50 text-black px-1 outline-none mb-6" type="name" placeholder="Name"
+                                    value={name}
+                                    onChange={(e) => setName(e.target.value)}
+                                />
+                                <input
+                                    className="w-full text-center rounded-2xl py-2 bg-gray-50 text-black px-1 outline-none mb-4" type="password" placeholder="Password"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
+
+                            </div>
+                        </form>
+                        <button className="bg-black text-white w-28 text-center rounded-full py-2 px-1 mb-8 hover:outline"
+                            disabled={secondOption === null}
+                            onClick={handleSubmit}
+                        >
+                            Submit
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
